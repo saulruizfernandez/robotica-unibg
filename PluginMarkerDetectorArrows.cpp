3fae628cae6e605167da17f4ec0f50aa04a89911
Saúl Ruiz Fernández <saul.ruiz.30@ull.edu.es>
2024-12-05 11:41:31 +0100

    Add files via upload

--- PluginMarkerDetectorArrows.cpp
+++ PluginMarkerDetectorArrows.cpp
@@ -59,6 +59,7 @@
 
 #include <iostream>
 #include <math.h>
+#include <algorithm>
 
 using namespace stresa;
 using namespace cv;
@@ -194,7 +195,7 @@ public:
 //				                      cv::Scalar(80, 60, 60), mask);
 
 		cv::inRange(frame, cv::Scalar(0, 0, 0),
-						                      cv::Scalar(80, 50, 50), mask);
+						                      cv::Scalar(128, 128, 128), mask);
         /*
 		cv::inRange(frame, cv::Scalar(0, 0, 80),
 						                        cv::Scalar(10, 20, 255), mask);
@@ -208,118 +209,132 @@ public:
 	}
 
 	void arrowDetection(cv::Mat &frame, std::vector<SceneTransform>& poses){
-
+		std::cout << ">>>>>>>> ARROW DETECTION\n";
 		Mat edges, output_image;
 
 		//	Canny( src_gray, edges, thresh, thresh*2 );
-		cv::Canny(frame, edges, 50, 200, 3, true);
+		cv::Canny(frame, edges, 100, 150, 3, true);
+		
+		imshow( "Canny", edges );
 
 		/*
 		 * Contour detection
 		 */
+		Mat drawing = Mat::zeros( edges.size(), CV_8UC3 );
 		vector<pair<Point, Point>>  arrow_vector;
 		vector<vector<Point> > contours, approx;
 		vector<Vec4i> hierarchy;
-		findContours( edges, contours, hierarchy, RETR_EXTERNAL, CHAIN_APPROX_SIMPLE );
-		Mat drawing = Mat::zeros( edges.size(), CV_8UC3 );
-
+		findContours( edges, contours, hierarchy, RETR_EXTERNAL , CHAIN_APPROX_SIMPLE );
+		
 		for( size_t i = 0; i< contours.size(); i++ ) {
-			vector<Point> tempApprox, hull;
-			double perimeter = cv::arcLength(contours[i], true);
-
-			if (perimeter <= 50){
-				continue;
-			}
-
-			double epsilon =  perimeter * 0.02;
-
-			cv::approxPolyDP(contours[i], tempApprox, epsilon, true);
-
-			if (tempApprox.size() == 7) {
-
-				cv::convexHull(tempApprox, hull, true);
-
-				if (7 - hull.size() == 2) { // It is an arrow, two inner points in the convex hull of 5 sides
-
-					double max1 =0; double max2 = 0;
-					std::pair<int, int> segment1, segment2;
-
-					for (size_t y = 0; y < hull.size(); y++){
-
-						cv::circle( drawing, hull[y], 10, (255,255,255), -1); // Show keypoints
-
-						double distance = cv::norm(hull[(y+1)%5] - hull[(y)]);
-						if (distance > max1 || distance > max2)
-						{
-							max2 = max1;
-							max1 = distance;
-
-							segment2 = segment1;
-							segment1 = make_pair(y, (y+1)%5);
-						}
-					}
-					int tip_index;
-					if((segment1.second + 2)% 5 == segment2.first) { // Find the tip of the arrow
-						tip_index = (segment1.second + 1) % 5;
-
-					}else if ((segment2.second + 2)% 5 == segment1.first){
-						tip_index = (segment2.second + 1)% 5;
-
-					}
-
-					// Calculate the first point of the vector that points to the direction of the tip
-					Point point1 = hull[(tip_index + 2) % 5];
-					Point point2 = hull[(tip_index + 3) % 5];
-
-					// Point middle_point
-					Point middle_point((point1.x + point2.x)/2, (point1.y + point2.y)/2);
-					
-					// Add new arrow vector
-					arrow_vector.push_back({hull[tip_index], middle_point});
-					
-					approx.push_back(tempApprox);
-					
-					// Calculate the center of the arrow
-					Point center_of_arrow;
-					Point point_tip = hull[tip_index];
-					Point point3 = hull[(tip_index + 1) % 5];
-					Point point4 = hull[(tip_index + 4) % 5];
-					
-					// Represent lines as ax + by = c
-					double a1 = point_tip.y - middle_point.y;
-					double b1 = middle_point.x - point_tip.x;
-					double c1 = a1 * (middle_point.x) + b1 * (middle_point.y);
-					
-					double a2 = point4.y - point3.y;
-					double b2 = point3.x - point4.x;
-					double c2 = a2 * (point3.x) + b2 * (point3.y);
-					
-					if ((a1 * b2 - a2 * b1) != 0) { // determinant != 0
-						center_of_arrow.x = (b2*c1 - b1*c2) / (a1 * b2 - a2 * b1);
-						center_of_arrow.y = (a1*c2 - a2*c1) / (a1 * b2 - a2 * b1);
-						// Draw circle in the intersection point
-						cv::circle(drawing, center_of_arrow, 10, Scalar(230, 237, 21), -1);
-					}
-					
-					// Calculate size of marker (height and width)
-					double height = sqrt((middle_point.x - point_tip.x) * (middle_point.x - point_tip.x) + (middle_point.y - point_tip.y) * (middle_point.y - point_tip.y));
-					double width = sqrt((point3.x - point4.x) * (point3.x - point4.x) + (point3.y - point4.y) * (point3.y - point4.y));
-					
-					std::cout << "SIZE OF MARKER:\n";
-					std::cout << "height: " << height << ", width: " << width << std::endl;
-					
-					// Calculate orientation of marker (yaw -> positive clockwise)
-					double slope_arrow_vector = -(a1 / b1);
-					double slope_horizontal_line = 0;
-					
-					double yaw_arrow = (atan(fabs((slope_arrow_vector - slope_horizontal_line) / (1 + slope_arrow_vector * slope_horizontal_line))) * 180) / M_PI;
-					std::cout << "orientation: " << yaw_arrow << "\n";
-					
-					
-				}
-			}
+			Scalar color = Scalar( 105, 105, 255 );
+//			Scalar color = Scalar( rng.uniform(0, 256), rng.uniform(0,256), rng.uniform(0,256) );
+			drawContours( drawing, contours, (int)i, color, 2, LINE_8, hierarchy, 0 );
 		}
 
+		for( size_t i = 0; i< contours.size(); i++ ) {
+			std::cout << "ping\n";
+			vector<Point> tempApprox;
+			vector<int> hull;
+			double perimeter = cv::arcLength(contours[i], true);
+
+			double epsilon =  perimeter * 0.025;
+
+			cv::approxPolyDP(contours[i], tempApprox, epsilon, true);
+			
+			for( size_t y = 0; y< tempApprox.size(); y++ ) {
+				Scalar color = Scalar( 255, 255, 255 );
+				//drawContours( drawing, tempApprox, (int)y, color, 2, LINE_8, hierarchy, 0 );
+				cv::circle(drawing, tempApprox[y], 4, color, -1);
+			}			
+
+			cv::convexHull(tempApprox, hull, false, false);
+			// Sort hull
+			std::sort(hull.begin(), hull.end());
+			std::cout << "pong1\n";
+
+			if ((hull.size()==5 ||  hull.size()==4) && (tempApprox.size() - hull.size()) == 2) { // It is an arrow, two inner points in the convex hull of 5 or 4 sides
+				
+				for (size_t y = 0; y < hull.size(); y++){
+					std::cout << hull[y]<<", ";
+				}
+				std::cout<<endl;
+				vector<int> indexes_tempApprox;
+				for (int i{0}; i < tempApprox.size(); ++i) indexes_tempApprox.push_back(i);
+				vector<int> indexes;
+				
+				std::set_difference(indexes_tempApprox.begin(), indexes_tempApprox.end(), hull.begin(), hull.end(), std::inserter(indexes, indexes.begin()));
+								
+				std::cout << "indexes: " << indexes[0] << ", " << indexes[1] << "\n"; 
+				
+				std::cout << "pong2\n";
+				int tip_index = -1;
+				for (int i{0}; i < 2; ++i) {
+					int x = (indexes[i] + 2) % hull.size();
+					int j = (indexes[(i+1)%2] - 2) < 0 ?  (hull.size() - abs(indexes[(i+1)%2]  - 2) % hull.size()) : (indexes[(i+1)%2] - 2);
+					std::cout << "X: "<<x<<", J:"<<j<<"\n";
+					if (j == x){
+						tip_index = j;
+					}
+				}
+				if (tip_index == -1){
+					std::cout<<"Tip not found\n";
+					continue;
+				}
+				std::cout << "pong3\n";
+
+				// Calculate the first point of the vector that points to the direction of the tip
+				Point point1 = tempApprox[hull[(tip_index + 2) % 5]];
+				Point point2 = tempApprox[hull[(tip_index + 3) % 5]];
+
+				// Point middle_point
+				Point middle_point((point1.x + point2.x)/2, (point1.y + point2.y)/2);
+				std::cout << "pong4\n";					
+				
+				// Add new arrow vector
+				arrow_vector.push_back({tempApprox[hull[tip_index]], middle_point});
+				std::cout << "pong5\n";	
+				
+				approx.push_back(tempApprox);
+				std::cout << "pong6\n";	
+				
+				// Calculate the center of the arrow
+				Point center_of_arrow;
+				Point point_tip = tempApprox[hull[tip_index]];
+				Point point3 = tempApprox[hull[(tip_index + 1) % 5]];
+				Point point4 = tempApprox[hull[(tip_index + 4) % 5]];
+				
+				// Represent lines as ax + by = c
+				double a1 = point_tip.y - middle_point.y;
+				double b1 = middle_point.x - point_tip.x;
+				double c1 = a1 * (middle_point.x) + b1 * (middle_point.y);
+				
+				double a2 = point4.y - point3.y;
+				double b2 = point3.x - point4.x;
+				double c2 = a2 * (point3.x) + b2 * (point3.y);
+				
+				if ((a1 * b2 - a2 * b1) != 0) { // determinant != 0
+					center_of_arrow.x = (b2*c1 - b1*c2) / (a1 * b2 - a2 * b1);
+					center_of_arrow.y = (a1*c2 - a2*c1) / (a1 * b2 - a2 * b1);
+					// Draw circle in the intersection point
+					cv::circle(drawing, center_of_arrow, 10, Scalar(230, 237, 21), -1);
+				}
+				
+				// Calculate size of marker (height and width)
+				double height = sqrt((middle_point.x - point_tip.x) * (middle_point.x - point_tip.x) + (middle_point.y - point_tip.y) * (middle_point.y - point_tip.y));
+				double width = sqrt((point3.x - point4.x) * (point3.x - point4.x) + (point3.y - point4.y) * (point3.y - point4.y));
+				
+				std::cout << "SIZE OF MARKER:\n";
+				std::cout << "height: " << height << ", width: " << width << std::endl;
+				
+				// Calculate orientation of marker (yaw -> positive clockwise)
+				double slope_arrow_vector = -(a1 / b1);
+				
+				double yaw_arrow = (atan(fabs(slope_arrow_vector)) * 180) / M_PI;
+				std::cout << "orientation: " << yaw_arrow << "\n";
+			}
+		}
+		std::cout << "pong\n";
 
 		for( size_t i = 0; i< approx.size(); i++ ) {
 			Scalar color = Scalar( 105, 105, 255 );
